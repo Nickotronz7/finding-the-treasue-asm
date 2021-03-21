4aaff222504ca1a972f5705f3134e21cfbff9860
Nickolas Rodriguez <nickotronz7@gmail.com>
2021-03-20 22:55:46 -0600

    print test

---
+++ print_lab/printLab.asm
@@ -0,0 +1,43 @@
+
+%include "systemcalls.asm"
+
+section .data
+    laberinto db "lab.txt",0
+ 
+ section .bss
+    text resb 1273
+ 
+section .text
+    global _start
+ 
+_start:
+    mov rax, SYS_OPEN
+    mov rdi, laberinto
+    mov rsi, O_RDONLY
+    mov rdx, 0
+    syscall
+   
+    push rax
+    mov rdi, rax
+    mov rax, SYS_LSEEK
+    mov rsi, 0 ; DESDE DONDE LOS TOMO 
+    mov rdx, 0
+    syscall
+ 
+    mov rax, SYS_READ
+    mov rsi, text
+    mov rdx, 1273
+    syscall
+
+    mov rax, SYS_CLOSE
+    pop rdi
+    syscall
+ 
+    mov rax, text
+    call print
+    
+    
+    mov rax, SYS_EXIT
+    mov rdi, 0
+    syscall
+   
