dd4fbf134745a60bab321edb25290a8caf90be3c
Joshua <joshua.moreira7@gmail.com>
2021-03-21 18:02:58 -0600

    Reset

--- main.asm
+++ main.asm
@@ -10,6 +10,10 @@ section .data
     MnsDif db "Elija la dificultad del juego",10,0
     MnsDif1 db "'d' para dificil 'f' para facil :",10,0
     MnsError db "Selecione una opcion valida",10,0
+    MnsReinicio db "Desea Reiniciar el juego?",10,0
+    MnsYN db "Para confirmar s para declinar n"
+    MnsFin db "Desea Finalizar el juego?",10,0
+    MnsFin1 db "Gracias por jugar!",10,0
 
  section .bss
     lab resb 1368
@@ -40,6 +44,22 @@ _dificil:
     load_Lab l2
     call _Init
     ;macro en dificil
+_reset:
+    print MnsReinicio
+    print MnsYN
+    getAction
+    mov rax,[action]
+    cmp rax,2675
+    je _start
+    cmp rax,2670
+    je _Play
+    print MnsError
+    jne _reset
 _Init: 
     writefilemac lab, 0, game, 1368
+_Play:
+    getAction;comparacion para realizar mov o r o f o l
+    mov rax,[action]
+    cmp rax,2674
+    je _reset
     exit
